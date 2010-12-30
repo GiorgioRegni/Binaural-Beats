74a814549bb5e4b233b297f4d0404671f271f381
Giorgio Regni <gr@3i7.net>
2010-12-30 14:54:52 +0100

    Aurora with glow

--- src/com/ihunda/android/binauralbeat/viz/Aurora.java
+++ src/com/ihunda/android/binauralbeat/viz/Aurora.java
@@ -6,6 +6,7 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Paint;
 
 import com.ihunda.android.binauralbeat.Visualization;
 
@@ -14,39 +15,31 @@ public class Aurora implements Visualization {
 	/**
 	 * Beat frequency in Hz
 	 */
-	double freq;
-	double period;
-	Bitmap background;
+	private double freq;
+	private double period;
+	private Bitmap background;
+	private Paint pTag;
 	
 	public Aurora() {
 		background = BitmapFactory.decodeResource(BBeat.getInstance().getResources(), R.drawable.aurora);
+		pTag = new Paint();
+		pTag.setStyle(Paint.Style.FILL);
 	}
 	
- 	
 	public void redraw(Canvas c, int width, int height, float now,
 			float totalTime) {
 		// window [-2.0, 1.0, -1.5, 1.5]
 		
-		double dperiod = period * 2 * 200;
+		double dperiod = period * 2 * 10;
 	
 		double ratio = (now % dperiod) / dperiod;
 		
-		/*
-		Bitmap bm = Bitmap.createBitmap(img, N, N, Bitmap.Config.ARGB_4444);
-		
-		// Paste
-		int wm = width/N;
-		double wmd = (double) width/N/wm;
-		int hm = height/N;
-		double hmd = (double) height/N/hm;
-		
-		for (int i=0; i< wm; i++) {
-			for (int j=0; j< hm; j++) {
-				c.drawBitmap(bm, (int) (i*N*wmd) , (int) (j*N*hmd), null);
-			}
-		}
-		*/
 		c.drawBitmap(background, 0, 0, null);
+		if (ratio > 0.5)
+			pTag.setColor(Color.argb((int) ((1-ratio)*75), 255, 255, 255));
+		else
+			pTag.setColor(Color.argb((int) (ratio*75), 255, 255, 255));
+		c.drawCircle(width/4, height/2, width/2, pTag);
 	}
 
 	public void setFrequency(float beat_frequency) {

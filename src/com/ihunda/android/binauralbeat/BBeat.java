b96ba8b6d4f8f4803b4491bad61a2cefe89be00b
Giorgio Regni <gr@3i7.net>
2011-01-02 12:04:18 +0100

    Added GPL V3 licensing terms and disclaimers

--- src/com/ihunda/android/binauralbeat/BBeat.java
+++ src/com/ihunda/android/binauralbeat/BBeat.java
@@ -1,5 +1,28 @@
 package com.ihunda.android.binauralbeat;
 
+/*
+ * @author Giorgio Regni
+ * @contact @GiorgioRegni on Twitter
+ * http://twitter.com/GiorgioRegni
+ * 
+ * This file is part of Binaural Beats Therapy or BBT.
+ *
+ *   BBT is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   BBT is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with BBT.  If not, see <http://www.gnu.org/licenses/>.
+ *   
+ *   BBT project home is at https://github.com/GiorgioRegni/Binaural-Beats
+ */
+
 import java.util.ArrayList;
 import java.util.Vector;
 

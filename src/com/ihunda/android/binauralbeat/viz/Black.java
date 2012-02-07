6b82c8b6f4d891af2b513a4a58833db4691a549f
Giorgio Regni <gr@3i7.net>
2012-02-07 22:38:27 +0100

    new viz

---
+++ src/com/ihunda/android/binauralbeat/viz/Black.java
@@ -0,0 +1,61 @@
+package com.ihunda.android.binauralbeat.viz;
+
+/*
+ * @author Giorgio Regni
+ * @contact @GiorgioRegni on Twitter
+ * http://twitter.com/GiorgioRegni
+ * 
+ * This file is part of Binaural Beats Therapy or BBT.
+ *
+ *   BBT is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   BBT is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with BBT.  If not, see <http://www.gnu.org/licenses/>.
+ *   
+ *   BBT project home is at https://github.com/GiorgioRegni/Binaural-Beats
+ */
+
+import android.graphics.Canvas;
+import android.graphics.Color;
+
+import com.ihunda.android.binauralbeat.CanvasVisualization;
+
+public class Black implements CanvasVisualization {
+
+	private static final int COLOR_BG = Color.rgb(0, 0, 0);
+	
+	/**
+	 * Beat frequency in Hz
+	 */
+	float freq;
+	float period;
+	float last = 0;
+	
+	public Black() {
+		last = 0;
+	}
+	
+	
+	public void redraw(Canvas c, int width, int height, float now,
+			float totalTime) {
+		if (last == 0 || now > last + 10)
+		{
+			c.drawColor(COLOR_BG);
+			last = now;
+		}	
+	}
+
+	public void setFrequency(float beat_frequency) {
+		freq = beat_frequency;
+		period = 1f / beat_frequency;
+	}
+
+}

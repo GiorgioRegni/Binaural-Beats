25bf54c54f35e322f48c5bf621c7c3d237df789f
Giorgio Regni <giorgio.regni@scality.com>
2016-08-27 16:56:17 -0700

    Added navigation drawer with blank layout

--- app/src/main/java/com/ihunda/android/binauralbeat/BBeat.java
+++ app/src/main/java/com/ihunda/android/binauralbeat/BBeat.java
@@ -53,6 +53,7 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.PowerManager;
 import android.os.SystemClock;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -408,6 +409,10 @@ public class BBeat extends AppCompatActivity {
 
         mToolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        NavigationDrawerFragment drawerFragment = (NavigationDrawerFragment) getSupportFragmentManager().findFragmentById(R.id.fragment_nav_drawer);
+        drawerFragment.setUp((DrawerLayout) findViewById(R.id.drawer_layout), mToolbar);
 
         showDialog(DIALOG_WELCOME);
 
---
+++ app/src/main/java/com/ihunda/android/binauralbeat/NavigationDrawerFragment.java
@@ -0,0 +1,61 @@
+package com.ihunda.android.binauralbeat;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link NavigationDrawerFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ */
+public class NavigationDrawerFragment extends Fragment {
+
+
+    private ActionBarDrawerToggle mDrawerToggle;
+    private DrawerLayout mDrawerLayout;
+
+
+    public NavigationDrawerFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_navigation_drawer, container, false);
+    }
+
+
+    public void setUp(DrawerLayout drawerlayout, Toolbar toolbar) {
+        mDrawerLayout = drawerlayout;
+        mDrawerToggle = new ActionBarDrawerToggle(getActivity(), mDrawerLayout, toolbar,
+                R.string.drawer_open,
+                R.string.drawer_close
+        ) {
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+            }
+        };
+
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+    }
+
+}

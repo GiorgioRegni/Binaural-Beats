a49e74977e708ddaeb5eb6385f4581de563221ac
Giorgio Regni <giorgio.regni@scality.com>
2016-08-27 17:28:32 -0700

    Added toggle to enable/disable drawer + state management

--- app/src/main/java/com/ihunda/android/binauralbeat/NavigationDrawerFragment.java
+++ app/src/main/java/com/ihunda/android/binauralbeat/NavigationDrawerFragment.java
@@ -1,9 +1,12 @@
 package com.ihunda.android.binauralbeat;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.SharedPreferencesCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
@@ -15,14 +18,20 @@ import android.view.ViewGroup;
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link NavigationDrawerFragment.OnFragmentInteractionListener} interface
  * to handle interaction events.
  */
 public class NavigationDrawerFragment extends Fragment {
 
 
+    private static final String PREF_FILE_NAME = "BBT_DRAWER";
+    private static final String KEY_USER_LEARNED_DRAWER = "user_learned_drawer";
     private ActionBarDrawerToggle mDrawerToggle;
     private DrawerLayout mDrawerLayout;
+
+    private boolean mUserLearnedDrawer;
+    private boolean mFromInstanceSaveState;
+
+    private View mContainerView;
 
 
     public NavigationDrawerFragment() {
@@ -37,8 +46,19 @@ public class NavigationDrawerFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_navigation_drawer, container, false);
     }
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mUserLearnedDrawer = Boolean.valueOf(readFromPreferences(getActivity(), KEY_USER_LEARNED_DRAWER, "false"));
+        if (savedInstanceState != null)
+            mFromInstanceSaveState = true;
+
+    }
 
     public void setUp(DrawerLayout drawerlayout, Toolbar toolbar) {
+
+        mContainerView = getActivity().findViewById(R.id.fragment_nav_drawer);
+
         mDrawerLayout = drawerlayout;
         mDrawerToggle = new ActionBarDrawerToggle(getActivity(), mDrawerLayout, toolbar,
                 R.string.drawer_open,
@@ -47,15 +67,44 @@ public class NavigationDrawerFragment extends Fragment {
             @Override
             public void onDrawerOpened(View drawerView) {
                 super.onDrawerOpened(drawerView);
+                if (!mUserLearnedDrawer) {
+                    mUserLearnedDrawer = true;
+                    saveToPreferences(getActivity(), KEY_USER_LEARNED_DRAWER, "true");
+                }
+                getActivity().invalidateOptionsMenu();
             }
 
             @Override
             public void onDrawerClosed(View drawerView) {
                 super.onDrawerClosed(drawerView);
+                getActivity().invalidateOptionsMenu();
             }
         };
 
+        if (!mUserLearnedDrawer && !mFromInstanceSaveState) {
+            mDrawerLayout.openDrawer(mContainerView);
+        }
+
         mDrawerLayout.setDrawerListener(mDrawerToggle);
+        mDrawerLayout.post(new Runnable() {
+            @Override
+            public void run() {
+                mDrawerToggle.syncState();;
+            }
+        });
+    }
+
+
+    public static void saveToPreferences(Context context, String prefName, String prefValue) {
+        SharedPreferences sharedPreferences = context.getSharedPreferences(PREF_FILE_NAME, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString(prefName, prefValue);
+        editor.commit();
+    }
+
+    public static String readFromPreferences(Context context, String prefName, String defaultValue) {
+        SharedPreferences sharedPreferences = context.getSharedPreferences(PREF_FILE_NAME, Context.MODE_PRIVATE);
+        return sharedPreferences.getString(prefName, defaultValue);
     }
 
 }

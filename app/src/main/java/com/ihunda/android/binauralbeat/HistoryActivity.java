5af612ab46003a3dad110530cd1c176dd66dc6a9
Giorgio Regni <giorgio@3i7.net>
2019-04-28 09:40:27 -0700

    Updated history to action bar

--- app/src/main/java/com/ihunda/android/binauralbeat/HistoryActivity.java
+++ app/src/main/java/com/ihunda/android/binauralbeat/HistoryActivity.java
@@ -2,8 +2,11 @@ package com.ihunda.android.binauralbeat;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.TextView;
 
@@ -12,16 +15,26 @@ import com.ihunda.android.binauralbeat.db.HistoryModel;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
-public class HistoryActivity extends Activity {
+public class HistoryActivity extends AppCompatActivity {
     private ArrayList<HistoryModel> arrayList;
     private HistoryAdapter adapter;
     private RecyclerView recyclerView;
     private TextView emptyTv;
 
+    Toolbar mToolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history);
+
+        mToolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        //NavigationDrawerFragment drawerFragment = (NavigationDrawerFragment) getSupportFragmentManager().findFragmentById(R.id.fragment_nav_drawer);
+        //drawerFragment.setUp((DrawerLayout) findViewById(R.id.drawer_layout), mToolbar);
+
         recyclerView = findViewById(R.id.recyclerView);
         emptyTv = findViewById(R.id.tvEmpty);
         try {
